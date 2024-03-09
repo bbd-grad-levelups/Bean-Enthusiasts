b53a8a7745a2ecbfa7a5d281beab1a3fe65cc98d
Derryk Fivaz <Derryk.Fivaz@bbd.co.za>
2024-03-09 13:05:52 +0200

    rename file

--- Server/src/main/java/com/bbd/BeanServer/controller/FavoriteBeansController.java
+++ Server/src/main/java/com/bbd/BeanServer/controller/FavoriteBeansController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-class BeansController {
+class FavoriteBeansController {
 
     @Autowired
     private FavoriteBeanRepository beansRepository;
