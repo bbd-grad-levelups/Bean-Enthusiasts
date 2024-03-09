f914696559a8eb4074b01a8b90620f1dfad19cfb
Derryk Fivaz <Derryk.Fivaz@bbd.co.za>
2024-03-09 13:01:43 +0200

    Get favourite beans controller, service, repo"

--- Server/src/main/java/com/bbd/BeanServer/controller/BeansController.java
+++ Server/src/main/java/com/bbd/BeanServer/controller/BeansController.java
@@ -1,12 +1,11 @@
 package com.bbd.BeanServer.controller;
 
-
 import com.bbd.BeanServer.assembler.ModelAssembler;
 import com.bbd.BeanServer.model.FavoriteBean;
-import com.bbd.BeanServer.repository.BeansRepository;
+import com.bbd.BeanServer.repository.FavoriteBeanRepository;
+import com.bbd.BeanServer.service.FavoriteBeanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
@@ -15,23 +14,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/favouritebeans")
 class BeansController {
 
     @Autowired
-    private BeansRepository beansRepository;
-
+    private FavoriteBeanRepository beansRepository;
+    @Autowired
+    private FavoriteBeanService favoriteBeanService;
 
     @Autowired
     private ModelAssembler<FavoriteBean> beansAssembler;
 
-    @GetMapping
-    public CollectionModel<EntityModel<FavoriteBean>> allBeans() {
-        List<EntityModel<FavoriteBean>> beans = beansRepository.findAll().stream()
-                .map(beansAssembler::toModel)
-                .collect(Collectors.toList());
-
-        return CollectionModel.of(beans);
+    @GetMapping("/favoritebeans")
+    CollectionModel<EntityModel<FavoriteBean>> all() {
+        List<EntityModel<FavoriteBean>> greetings = favoriteBeanService.getAllFavoriteBeans().stream()
+                .map(beansAssembler::toModel).collect(Collectors.toList());
+        return CollectionModel.of(greetings);
     }
 
 
--- Server/src/main/java/com/bbd/BeanServer/repository/FavoriteBeanRepository.java
+++ Server/src/main/java/com/bbd/BeanServer/repository/FavoriteBeanRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bbd.BeanServer.model.FavoriteBean;
 
-public interface BeansRepository extends JpaRepository<FavoriteBean, Long>{
+public interface FavoriteBeanRepository extends JpaRepository<FavoriteBean, Long>{
 
 }
 
