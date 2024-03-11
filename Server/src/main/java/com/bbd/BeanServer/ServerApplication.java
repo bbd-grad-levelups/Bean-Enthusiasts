52f33c42419fb2216f1e68e5c73b5e4c3332c393
jhviljoen <johan.viljoen@bbd.co.za>
2024-03-11 15:51:10 +0200

    Small fix for ModelAssembler move

--- Server/src/main/java/com/bbd/BeanServer/ServerApplication.java
+++ Server/src/main/java/com/bbd/BeanServer/ServerApplication.java
@@ -4,10 +4,12 @@ package com.bbd.BeanServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
-@EntityScan("com.bbd.shared.models")
+@EntityScan({"com.bbd.BeanServer", "com.bbd.shared"})
+@ComponentScan({"com.bbd.BeanServer", "com.bbd.shared"})
 public class ServerApplication {
   
   public static void main(String... args) {
